df75d3724848cb8ebab3a6996ef7b026c80117f3
Jordan Morrison <jordanmorrison476@gmail.com>
2020-12-09 02:56:11 +0000

    Need to find of bulk deleting Spotify Playlists programatically

--- data-parse/parse.py
+++ data-parse/parse.py
@@ -1,12 +1,8 @@
-# Python program to read 
-# json file 
-  
-  
 import json 
 from extract import json_extract
-  
+''' To-Do: import all of your playlists into seperate JSON files manually and conc into a longer dict '''
 # Opening JSON file 
-f = open('C:/Users/Jorda/Desktop/Python/Learning Basics - Delete Spotify Playlists/jordanPlaylists2020.json',) 
+f = open('C:/Users/Jorda/Desktop/Python/Learning Basics - Delete Spotify Playlists/data-parse/jordanPlaylists2020.json')
   
 # returns JSON object as  
 # a dictionary 
@@ -15,73 +11,35 @@ data = json.load(f)
 # Closing file 
 f.close()
 
-data
-
-#print(data)
-
 # Find every instance of `name` in a Python dictionary.
 items = json_extract(data, 'uri')
-
-items
-
-# print(items)
+playlistNames = json_extract(data, 'name')
 
 playlistURls = items[1::2]  # Get every even-index value from a list
 userProfileURLs = items[2::1]  # Get every odd-index value from a list
 
-print('Playlists URLS = ', playlistURls)
+''' what are you trying to do here? '''
+''' get all playlistURIs where name of playlist = My Setlist  '''
 
-''' now that you have all of your playlist urls in one nice array, delete the unwanted ones from Spotify programatically using the api '''
-''' how do you know which playlists to delete hmmmmm? '''
-''' either use a UI to group select playlists or some other way.... '''
+inputPlaylistName = input("Enter Playlist Name: ")
 
+upprBound = len(playlistURls)
 
+arr = []
 
-
-
-
-
-
-
-
-
-
-
-
-# str_playlist_DICTIONARY = json.dumps(data, indent=2)
-
-# #print(playlist_DICTIONARY)
-
-# # Iterating through the json 
-# # list 
-
-# playlistItems_LIST = []
-
-# for i in data['items']: 
-#     playlistItems_LIST.append(i)
-
-# # print(playlist_DICTIONARY)
-
-
-
-
-
-
-
-
-
-
-# # using json.loads() 
-# # convert dictionary string to dictionary 
-# res = json.loads(str_playlist_DICTIONARY) 
-  
-# # print result 
-# # print("The converted dictionary : " + str(res)) 
-
-# # Find every instance of `name` in a Python dictionary.
-# items = json_extract(str_playlist_DICTIONARY.json(), 'items')
-# print(items)
-
-
-
-
+for x in range(0, upprBound):
+    arr.append({"name": playlistNames[x], "value" : playlistURls[x]})
+    
+for x in arr:
+    if x["name"] == inputPlaylistName:
+        chosenToDeletev1 = x["value"]
+        y = chosenToDeletev1.split(":")
+        print(y[2])
+ 
+''' another problem... '''
+''' Spotify doesn't let you bulk unfollow playlists using their web console ffs '''
+''' Now import a Spotipy library to handle auth '''
+''' declare a const array of playlists I want to keep '''
+''' find all playlistUris where playlist name is not in the saved playlists array '''
+''' iterate over all unwanted playists and unfollow each playlist individually ''' 
+''' Show a deleteUnwantedPlaylist confirmation message '''
